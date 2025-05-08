4f8a3daa6a0ed318455de3e00c8c591e20f06b95
unalkyouie <37963200+unalkyouie@users.noreply.github.com>
2025-05-08 18:29:37 +0200

    chore: add and setup react-query (#1)
    * chore: add react-query
    
    * chore: add and integrate QueryClientProvider

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 
 import {MainStack} from './src/stacks/Main';
+import QueryProvider from './src/services/QueryClientProvider';
 
 function App(): React.JSX.Element {
+
+  
   return (
-    <NavigationContainer>
-      <MainStack />
-    </NavigationContainer>
+    <QueryProvider>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
+    </QueryProvider>
   );
 }
 
