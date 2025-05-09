68c202db43ea7e2f66e4735d5180e5e613d9649f
Aleksandra Pikus <alk.pikus@gmail.com>
2025-05-09 13:03:18 +0200

    feat: create checkbox component

---
+++ src/components/Checkbox/Checkbox.styled.tsx
@@ -0,0 +1,19 @@
+import {
+
+    StyleSheet,
+
+  } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    label: {
+      marginLeft: 8,
+      fontSize: 14,
+    },
+  });
+  
+  
